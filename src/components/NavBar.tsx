ba3fb58e56514f9d3d77a434c696a791ea396e41
Alsten Dsouza <alstendsouza18@gmail.com>
2025-10-09 12:45:27 -0700

    Added logo to navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="64px" />
+      <Text>Navbar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
