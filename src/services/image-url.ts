c21a5c6ab3cd9078d9323baadf082fec48efc096
Alsten Dsouza <alstendsouza18@gmail.com>
2025-10-22 12:59:36 -0700

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 const getCroppedImageUrl = (image: string) => {
-    if (!image) return "";
+    if (!image) return noImage;
     const target = "media/";
     const index = image.indexOf(target) + target.length;
     return image.slice(0, index) + 'crop/600/400/' + image.slice(index);
