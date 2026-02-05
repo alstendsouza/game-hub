cc2618ba4dafc891c8d3207dde9830f5b03095f2
adsouza <adsouza@deltacontrols.com>
2026-02-05 05:45:04 -0800

    installed react query and react query devtools and updated useGenres hook to use react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,8 @@
 
 import genres from "../data/genres";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 export interface Genre {
     id: number;
     name: string;
@@ -8,6 +11,13 @@ export interface Genre {
 }
   
 
-const useGenres = () =>({data:genres, isLoading:false, error:null});
+const useGenres = () => useQuery({
+    queryKey: ['genres'],
+    queryFn: () => {
+       return apiClient.get<FetchResponse<Genre>>('/genres').then(res => res.data)
+    },
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: genres.length, results: genres }
+});
 
 export default useGenres;
