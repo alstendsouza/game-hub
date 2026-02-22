0c4232986eb67df37d5fa3e5cb2c22861c9ff829
adsouza <adsouza@deltacontrols.com>
2026-02-22 02:27:22 -0800

    Replaced hardcoded staleTime values with ms library for better readability

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,6 +2,7 @@
 import genres from "../data/genres";
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import ms from "ms";
 
 const apiClient = new APIClient<Genre>('/genres');
 
@@ -16,7 +17,7 @@ export interface Genre {
 const useGenres = () => useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms('24h'),
     initialData: genres
 });
 
